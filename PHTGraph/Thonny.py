9ba273cbb6afaebeb85ad371cc8095473bdfe3a2
irunthelaw <2026phillips.junior@fcboe.org>
2025-02-03 13:23:17 -0500

    Update Thonny.py

--- PHTGraph/Thonny.py
+++ PHTGraph/Thonny.py
@@ -1,8 +1,13 @@
 import board
 import time
 from adafruit_ms8607 import MS8607
-pht = MS8607(i2c)
+
+
 i2c = board.I2C()
+pht = MS8607(i2c)
+
 while True:
-      print(pht.pressure, ", ", (int(pht.relative_humidity * 100) / 100) ,", ",  (int(((9/5) * pht.temperature + 32) * 100)) / 100)
+      print(pht.pressure,
+            (int(pht.relative_humidity * 100) / 100) ,
+            (int(((9/5) * pht.temperature + 32) * 100)) / 100)
       time.sleep(5)
